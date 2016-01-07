04c0b4892c735b8427773f727dff0dedbdb4ccf6
Daniel Wagner-Hall <dawagner@gmail.com>
2016-01-07 00:56:06 +0000

    Test pastScore

--- FranksZookeeperTests/PlayerTest.swift
+++ FranksZookeeperTests/PlayerTest.swift
@@ -27,4 +27,16 @@ class PlayerTest: XCTestCase {
         p.scores = [2, 4, 6]
         XCTAssertEqual(12, p.score)
     }
+
+    func testPastScores_Many() {
+        let p = Player(name: "Cat")
+        p.scores = [2, 4, 6]
+        XCTAssertEqual(12, p.pastScore(0))
+        XCTAssertEqual(6, p.pastScore(1))
+        XCTAssertEqual(2, p.pastScore(2))
+        XCTAssertEqual(0, p.pastScore(3))
+        XCTAssertEqual(0, p.pastScore(4))
+        XCTAssertEqual(12, p.pastScore(0))
+        XCTAssertEqual(12, p.score)
+    }
 }
